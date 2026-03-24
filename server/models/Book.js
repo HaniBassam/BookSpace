857f96bae022a95aa4c8e5b0a52ec9e74df2756e
HaniB <143654847+HaniBassam@users.noreply.github.com>
2026-03-24 19:18:10 +0100

    Add Book schema with extended fields (ratings, language, relations)

---
+++ server/models/Book.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+
+const bookSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        author: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        genre: {
+            type: String,
+            trim: true,
+        },
+        description: {
+            type: [String],
+            default: [],
+        },
+        coverImageUrl: {
+            type: String,
+            default: "",
+        },
+        pageCount: {
+            type: Number,
+            required: true,
+            min : 1,
+        },
+        publishedYear: {
+            type: Date,
+            min: 0,
+        },
+        language: {
+            type: String,
+            default: "English"
+        },
+        avarageRating: {
+            type: Number,
+            default: 0,
+            min: 0,
+            max: 5,
+        },
+        reviews: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Review"
+            },
+        ],
+    },
+    { timestamps: true }
+);
+
+const Book = mongoose.model("Book", bookSchema);
+
+export default Book;
