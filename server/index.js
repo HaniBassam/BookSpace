2bd990659bc89c61a546d50b493625d2ed52f7a2
HaniB <143654847+HaniBassam@users.noreply.github.com>
2026-03-25 19:40:40 +0100

    Add books API route

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import Book from "./models/Book.js";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -19,6 +20,19 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
+app.get("/books", async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.json(books);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ message: error.message });
+    } else {
+      res.status(500).json({ message: "Failed to fetch books" });
+    }
+  }
+})
 
 async function startServer() {
   try {
